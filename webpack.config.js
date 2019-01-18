1581608ac30b5d1b2770187469407d9d0a70912c
w <jwj3678@gmail.com>
2019-01-18 21:01:20 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/main.js',
+	},
+	output: {
+		path: path.resolve(__dirname,'public/dist'),
+		filename: '[name].js',
+		publicPath: "/",
+	},
+	resolve: {
+		modules: ['node_modules'],
+		extensions: ['.js', '/jsx', '.json', '.css']
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			Popper: ['popper.js', 'default']
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	module: {
+		rules: [{
+			test: /\.(scss)$/,
+			use: [
+				{
+					 loader: "style-loader" 
+				},
+				{
+					loader: "css-loader"
+				},
+				{
+					loader: "postcss-loader",
+					options: {
+						plugins: function() {
+							return [
+								require( 'autoprefixer' )
+							]
+						}
+					}
+				},
+				{
+					loader: "sass-loader"
+				}
+			]
+		}]
+	},
+	devServer: {
+		contentBase: __dirname + '/public/',
+		inline: true,
+		hot: true,
+		host: '0.0.0.0',
+		port:3000
+	}
+};
+
